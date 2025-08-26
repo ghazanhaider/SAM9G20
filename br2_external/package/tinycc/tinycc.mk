ea0cce69e0ef56cc0862a038a47fe1b29a83cdc8
Ghazan Haider <ghazan.haider@gmail.com>
2025-08-26 03:24:52 +0000

    made tinycc work! except for modulus operator ??

--- br2_external/package/tinycc/tinycc.mk
+++ br2_external/package/tinycc/tinycc.mk
@@ -8,28 +8,12 @@ TINYCC_VERSION = release_0_9_27
 TINYCC_SITE = $(call github,TinyCC,tinycc,$(TINYCC_VERSION))
 TINYCC_LICENSE = LGPL
 TINYCC_LICENSE_FILE = RELICENSING
-TINYCC_INSTALL_STAGING = YES
+TINYCC_INSTALL_STAGING = NO
 TINYCC_INSTALL_TARGET = YES
-TINYCC_CONF_OPTS += --cpu=arm --triplet=arm-linux-gnu
+TINYCC_CONF_OPTS += --cpu=arm --triplet=arm-linux-gnu --cc=$(TARGET_CROSS)gcc --ar=$(TARGET_CROSS)ar --config-musl --crtprefix=/usr/lib
+TINYCC_MAKE_OPTS += arm-libtcc1-usegcc=yes
 
-#define TINYCC_BUILD_CMDS
-#        $(TARGET_MAKE_ENV) $(MAKE) CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D)
-#endef
+#TINYCC_CONF_OPTS += --cpu=armv5tejl --triplet=arm-linux-gnu --cc=$(TARGET_CROSS)gcc --ar=$(TARGET_CROSS)ar --config-musl --tcc_sysincludepaths=/usr/include --tcc_libpaths=/lib --crtprefix=/usr/lib
 
-#define TINYCC_INSTALL_STAGING_CMDS
-#        $(TARGET_MAKE_ENV) $(MAKE) \
-#                -C $(@D) \
-#                PREFIX="/usr" \
-#                CROSS_COMPILE="$(TARGET_CROSS)" \
-#                DESTDIR="$(STAGING_DIR)" install
-#endef
-#
-#define TINYCC_INSTALL_TARGET_CMDS
-#        $(TARGET_MAKE_ENV) $(MAKE) \
-#                -C $(@D) \
-#                PREFIX="/usr" \
-#                CROSS_COMPILE="$(TARGET_CROSS)" \
-#                DESTDIR="$(TARGET_DIR)" install
-#endef
 
 $(eval $(autotools-package))
