6e3b7a9d0233c68f9029659c8a6c8ef5fc4a1841
Ghazan Haider <ghazan.haider@gmail.com>
2025-09-02 15:52:52 +0000

    Working gpio.c demo and more instruments.. strace still doesnt work

--- br2_external-2019.02.11/fs_overlay/root/dev/gpio.c
+++ br2_external-2019.02.11/fs_overlay/root/dev/gpio.c
@@ -6,20 +6,23 @@
 
 int main(void) {
     gpio_t gpio_out;
-    bool value;
+    bool value=0;
+    int x;
 
 
-    /* Open GPIO B9 which is B (32) + 8 = 40 with output direction */
-    if (gpio_open(&gpio_out, 72, GPIO_DIR_OUT) < 0) {
+    if (gpio_open(&gpio_out, 73, GPIO_DIR_OUT) < 0) {
         fprintf(stderr, "gpio_open(): %s\n", gpio_errmsg(&gpio_out));
         exit(1);
     }
 
 
-    /* Write output GPIO with !value */
-    if (gpio_write(&gpio_out, !value) < 0) {
-        fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(&gpio_out));
-        exit(1);
+    for (x=0;x<10;x++) {
+        if (gpio_write(&gpio_out, !value) < 0) {
+            fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(&gpio_out));
+            exit(1);
+        }
+        value = !value;
+        sleep(1);
     }
 
     gpio_close(&gpio_out);
