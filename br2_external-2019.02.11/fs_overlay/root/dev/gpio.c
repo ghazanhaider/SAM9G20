4eb1344a4fb4fd7c2aa6c64e55074e3710b445c7
Ghazan Haider <ghazan.haider@gmail.com>
2025-09-02 14:26:15 +0000

    fully instrumented 2.6.32, minus perf

--- br2_external-2019.02.11/fs_overlay/root/dev/gpio.c
+++ br2_external-2019.02.11/fs_overlay/root/dev/gpio.c
@@ -5,41 +5,23 @@
 #include "gpio.h"
 
 int main(void) {
-    gpio_t *gpio_in, *gpio_out;
-    bool value=0;
+    gpio_t gpio_out;
+    bool value;
 
-/*    gpio_in = gpio_new(); */
-    gpio_out = gpio_new();
 
-    /* Open GPIO /dev/gpiochip1 line 9 with input direction */
-/*    if (gpio_open(gpio_in, "/dev/gpiochip1", 9, GPIO_DIR_IN) < 0) {
-        fprintf(stderr, "gpio_open(): %s\n", gpio_errmsg(gpio_in));
-        exit(1);
-    } */
-
-    /* Open GPIO /dev/gpiochip0 line 12 with output direction */
-    if (gpio_open(gpio_out, "/dev/gpiochip0", 12, GPIO_DIR_OUT) < 0) {
-        fprintf(stderr, "gpio_open(): %s\n", gpio_errmsg(gpio_out));
+    /* Open GPIO B9 which is B (32) + 8 = 40 with output direction */
+    if (gpio_open(&gpio_out, 72, GPIO_DIR_OUT) < 0) {
+        fprintf(stderr, "gpio_open(): %s\n", gpio_errmsg(&gpio_out));
         exit(1);
     }
 
-    /* Read input GPIO into value */
-/*    if (gpio_read(gpio_in, &value) < 0) {
-        fprintf(stderr, "gpio_read(): %s\n", gpio_errmsg(gpio_in));
-        exit(1);
-    } */
 
     /* Write output GPIO with !value */
-    if (gpio_write(gpio_out, !value) < 0) {
-        fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(gpio_out));
+    if (gpio_write(&gpio_out, !value) < 0) {
+        fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(&gpio_out));
         exit(1);
     }
 
-/*    gpio_close(gpio_in); */
-    gpio_close(gpio_out);
-
-/*    gpio_free(gpio_in); */
-    gpio_free(gpio_out);
-
+    gpio_close(&gpio_out);
     return 0;
 }
